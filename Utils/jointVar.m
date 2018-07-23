9578734b77ba4f6b02a8f2fee91e21728e79b403
Yaocoo <yaocong.duan@gmail.com>
2018-07-23 16:51:20 +0100

    update jointVar

--- Utils/jointVar.m
+++ Utils/jointVar.m
@@ -1,4 +1,4 @@
-function [ JXY ] = jointVar( X, Y )
+function [ JXY, values ] = jointVar( X, Y )
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Description: generate the joint variable of variables X and Y.
 %
@@ -8,6 +8,8 @@ function [ JXY ] = jointVar( X, Y )
 %   Y - A column vector.
 % Output:
 %   JXY - The joint variable.
+%   values - a maxtrix of joint variable values. the joint value of X = 2
+%   and Y = 3 is values(2,3).
 %
 % Date: 2018/05/17
 % Revision: 2018/05/17
@@ -19,11 +21,13 @@ function [ JXY ] = jointVar( X, Y )
 xUniq = unique(X)';
 yUniq = unique(Y)';
 JXY = nan(Ntrl,1);
+values = nam(max(xUniq), max(yUniq));
 
 label = 1;
 for xv = xUniq
     for yv = yUniq
         JXY(X == xv & Y == yv) = label;
+        values(xv, yv) = label;
         label = label + 1;
     end
 end
